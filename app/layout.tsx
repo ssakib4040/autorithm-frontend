a2fea2b8ae7364d61c1b0d82e61c2bcee10d201e
Sadman Sakib <ssakib4040@gmail.com>
2026-01-27 03:52:38 +0600

    Vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+
 import "../styles/globals.css";
 import AuthProvider from "./AuthProvider";
 
@@ -30,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>{children}</AuthProvider>
+        <Analytics />
       </body>
     </html>
   );
